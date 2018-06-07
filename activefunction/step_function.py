c1c6fde13bdeec7c00bfe46dfab99bf272fe3ed2
nozomi0966 <nozomotoitech@gmail.com>
2018-06-07 17:58:19 +0900

    update step func

--- activefunction/step_function.py
+++ activefunction/step_function.py
@@ -1,6 +1,13 @@
-def step_func(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
+import numpy as np
+import matplotlib.pylab as plt
 
+def step_func(x):
+    y = x > 0
+    return y.astype(int)
+
+if __name__ == "__main__":
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_func(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
